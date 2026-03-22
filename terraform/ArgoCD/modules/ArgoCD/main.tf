866ab6fd5c1a1434e45300dea07124db63a99250
Ryzhankou <v.ryzhenkov@gmail.com>
2026-03-22 12:22:16 +0100

    Update helm

--- terraform/ArgoCD/modules/ArgoCD/main.tf
+++ terraform/ArgoCD/modules/ArgoCD/main.tf
@@ -1,16 +1,3 @@
-# resource "helm_release" "ArgoCD" {
-#     name       = "argocd"
-#     repository = "https://argoproj.github.io/argo-helm"
-#     chart      = "argo-cd"
-#     namespace  = "argocd"
-#     wait = true
-#     create_namespace = true
-
-#     values = [
-#         file("${path.module}/files/argocd-values.yaml")
-#   ]
-# }
-
 resource "helm_release" "ArgoCD" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
--- terraform/ArgoCD/providers.tf
+++ terraform/ArgoCD/providers.tf
@@ -1,17 +1,7 @@
-# provider "kubernetes" {
-#   alias = "cluster"
-#   config_path = "~/.kube/config"
-#   # host                   = var.EKS_required ? data.aws_eks_cluster.eks_cluster.endpoint : null
-#   # cluster_ca_certificate = var.EKS_required ? base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data) : null
-#   # token                  = var.EKS_required ? data.aws_eks_cluster_auth.eks.token : null
-# }
-
 provider "helm" {
     kubernetes {
         config_path = "~/.kube/config"
         config_context = "kind-dev-global-cluster-0"
-        # host                   = var.EKS_required ? data.aws_eks_cluster.eks_cluster.endpoint : null
-        # cluster_ca_certificate = var.EKS_required ? base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data) : null
-        # token                  = var.EKS_required ? data.aws_eks_cluster_auth.eks.token : null
+
     }
 }
